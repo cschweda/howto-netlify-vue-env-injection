f8bb2a9f1d8788a8c75f0368a386d20b6a7716d7
Chris Schweda <cschweda@gmail.com>
2018-01-19 10:52:18 -0600

    dd prettier / remove error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -49,42 +49,34 @@
 </template>
 
 <script>
-  var Prism = require('prismjs');
-
-
-  console.log(process.env.API_KEY)
-  export default {
-    name: 'HelloWorld',
-    mounted: function () {
-      console.log(process.env.API_KEY)
-
-    },
-    data() {
-      return {
-        msg: process.env.NODE_ENV,
-        apiKey: process.env.API_KEY,
-        step1: ''
-      }
-    }
+export default {
+  name: "HelloWorld",
+  mounted: function() {},
+  data() {
+    return {
+      msg: process.env.NODE_ENV,
+      apiKey: process.env.API_KEY
+    };
   }
+};
 </script>
 
 <!-- Add "scoped " attribute to limit CSS to this component only -->
 <style scoped>
-  a {
-    color: #42b983;
-  }
+a {
+  color: #42b983;
+}
 
-  #hello {
-    text-align: center
-  }
+#hello {
+  text-align: center;
+}
 
-  h1 {
-    font-size: 24px
-  }
+h1 {
+  font-size: 24px;
+}
 
-  .key {
-    font-weight: bold;
-    color: #3e9b50
-  }
+.key {
+  font-weight: bold;
+  color: #3e9b50;
+}
 </style>
